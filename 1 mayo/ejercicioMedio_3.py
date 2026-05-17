c7c8c02ff70941fdc06cff76930cff3e0fe72a37
Richie0240 <ricardoalpizar23@gmail.com>
2026-05-17 12:39:10 -0600

    Ejercicios medios hechos

--- 1 mayo/ejercicioMedio_3.py
+++ 1 mayo/ejercicioMedio_3.py
@@ -18,17 +18,64 @@
 
 
 notas = []
-resultado = 0
-promedio = 0
 
 
-def pedirCargarNota(cantidad):
-    while cantidad < 5:
-        nota = int(input("Ingrese la nota del estudiante: "))
+def cargar_notas(cantidad):
+
+    contador = 0
+
+    while contador < cantidad:
+
+        try:
+            nota = int(input(f"Ingrese la nota del estudiante #{contador + 1}: "))
+
+        except ValueError:
+            print("Debe ingresar un número válido\n")
+            continue
+
         notas.append(nota)
-        print("Nota agregada correctamente")
-    print("Notas agregadas:")
-    print(notas)
+
+        print("Nota agregada correctamente\n")
+
+        contador += 1
+
+    return notas
 
 
-pedirCargarNota()
+def calcularPromedio(notas):
+    suma = 0
+    for i in notas:
+        suma += i
+    promedio = suma / len(notas)
+    return promedio
+
+
+def encontrarMenor(notas):
+    notaMenor = min(notas)
+    notaMayor = max(notas)
+    return notaMenor, notaMayor
+
+
+def aprobaron(notas):
+    estudiantesAprobados = 0
+    for i in notas:
+        if i >= 70:
+            estudiantesAprobados += 1
+
+    reprobados = len(notas) - estudiantesAprobados
+
+    return estudiantesAprobados, reprobados
+
+
+cargar_notas(5)
+
+notaMenor, notaMayor = encontrarMenor(notas)
+estudiantesAprobados, reprobados = aprobaron(notas)
+print("El promedio de las notas es: ", calcularPromedio(notas))
+print("La nota mas baja es: ", notaMenor, " y la nota mayor es: ", notaMayor)
+print(
+    "La cantidad de estudiantes aprobados: ",
+    estudiantesAprobados,
+    " y la cantidad de estudiantes reprobados: ",
+    reprobados,
+)
